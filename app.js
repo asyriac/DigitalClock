f7f813be63d6d5a7ffbb67323c6f62d672c06a16
asyriac <43615822+asyriac@users.noreply.github.com>
2019-07-06 23:16:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const sec = document.querySelector('.time');
+// const hr = document.querySelector('.hours');
+// const mn = document.querySelector('.minutes');
 const time = setInterval(()=>{
   const now = new Date();
-  sec.innerText = `${now.getHours()<10) ? ("0" + now.getHours()) : now.getHours()}:${(now.getMinutes()<10) ? ("0" + now.getMinutes()) : now.getMinutes()}:${(now.getSeconds()<10) ? ("0" + now.getSeconds()) : now.getSeconds()}`
+  sec.innerText = `${(now.getHours()<10) ? ("0" + now.getHours()) : now.getHours()}:${(now.getMinutes()<10) ? ("0" + now.getMinutes()) : now.getMinutes()}:${(now.getSeconds()<10) ? ("0" + now.getSeconds()) : now.getSeconds()}`
 },10);
